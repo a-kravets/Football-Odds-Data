c552cdda111c0bd2bade0af229115d4933ff7eb5
Andriy Kravets <a-kravets@users.noreply.github.com>
2021-03-08 17:44:29 +0200

    adding beta_expanders

--- football_data_odds.py
+++ football_data_odds.py
@@ -148,9 +148,20 @@ if add_selectbox != '--- all teams ---':
     data_show_selected_team = data_show_selected_team[['Date', 'HomeTeam', 'AwayTeam', 'Result', 'AvgH', 'AvgD', 'AvgA', 'Avg>2.5', 'Avg<2.5']]
     if st.checkbox("Show raw data on games and odds in table format"): 
         st.write(data_show_selected_team.sort_values(by=['Date']))
+        st.text('You may sort table results by clicking the relevant column name')
+        st.text('*AvgH = Market average home win odds')
+        st.text('*AvgD = Market average draw win odds')
+        st.text('*AvgA = Market average away win odds')
+        st.text('*Avg>2.5 = Market average over 2.5 goals')
+        st.text('*Avg<2.5 = Market average under 2.5 goals')
 else:
     st.write(data_show)
-
+    st.text('You may sort table results by clicking the relevant column name')
+    st.text('*AvgH = Market average home win odds')
+    st.text('*AvgD = Market average draw win odds')
+    st.text('*AvgA = Market average away win odds')
+    st.text('*Avg>2.5 = Market average over 2.5 goals')
+    st.text('*Avg<2.5 = Market average under 2.5 goals')
 
 
 
@@ -159,142 +170,135 @@ if add_selectbox != '--- all teams ---':
     selected_team_draw = filtered_data_final
     selected_team_away = filtered_data_final[filtered_data_final['AwayTeam'] == add_selectbox]
 
-    st.header('HomeWin, Draw & AwayWin')    
 
-    chart_home_selected = alt.Chart(selected_team_home).mark_bar(size=10).encode(
-        x='Date',
-        y='BetWinHome',
-        color=alt.condition(
-            alt.datum.BetWinHome > 0,
-            alt.value("green"),  # The positive color
-            alt.value("red")  # The negative color
-        #color='animal',
-        # legend=alt.Legend(title='Animals by year')
-    ))
-    st.subheader('What if you bet $100 on home team wins in all ' + add_selectbox + ' home games?')
-    st.text('You would got ' + str(selected_team_home['BetWinHome'].sum().astype(int)) + ' USD') 
-    st.altair_chart(chart_home_selected, use_container_width=True)
- 
-
-    # Basic Altair line chart where it picks automatically the colors for the lines
-    chart_draw_selected = alt.Chart(selected_team_draw).mark_bar(size=10).encode(
-        x='Date',
-        y='BetDraw',
-        color=alt.condition(
-            alt.datum.BetDraw > 0,
-            alt.value("green"),  # The positive color
-            alt.value("red")  # The negative color
-        #color='animal',
-        # legend=alt.Legend(title='Animals by year')
-    ))
-    st.subheader('What if you bet $100 on draws in all ' + add_selectbox + ' games?')
-    st.text('You would got ' + str(selected_team_draw['BetDraw'].sum().astype(int)) + ' USD')   
-    st.altair_chart(chart_draw_selected, use_container_width=True)
-
-
-    chart_away_selected = alt.Chart(selected_team_away).mark_bar(size=10).encode(
-        x='Date',
-        y='BetWinAway',
-        color=alt.condition(
-            alt.datum.BetWinAway > 0,
-            alt.value("green"),  # The positive color
-            alt.value("red")  # The negative color
-        #color='animal',
-        # legend=alt.Legend(title='Animals by year')
-    ))
-    st.subheader('What if you bet $100 on away team wins in all ' + add_selectbox + ' away games?')
-    st.text('You would got ' + str(selected_team_away['BetWinAway'].sum().astype(int)) + ' USD') 
-    st.altair_chart(chart_away_selected, use_container_width=True)
-
-
-    st.header('Total Goals')
-
-    chart_home_selected = alt.Chart(selected_team_home).mark_bar(size=10).encode(
-        x='Date',
-        y='BetLess25',
-        color=alt.condition(
-            alt.datum.BetLess25 > 0,
-            alt.value("green"),  # The positive color
-            alt.value("red")  # The negative color
-        #color='animal',
-        # legend=alt.Legend(title='Animals by year')
-    ))
-    st.subheader('What if you bet $100 on total goals of < 2.5 in all ' + add_selectbox + ' home games?')
-    st.text('You would got ' + str(selected_team_home['BetLess25'].sum().astype(int)) + ' USD') 
-    st.altair_chart(chart_home_selected, use_container_width=True)
-
-
-    chart_home_selected = alt.Chart(selected_team_home).mark_bar(size=10).encode(
-        x='Date',
-        y='BetMore25',
-        color=alt.condition(
-            alt.datum.BetMore25 > 0,
-            alt.value("green"),  # The positive color
-            alt.value("red")  # The negative color
-        #color='animal',
-        # legend=alt.Legend(title='Animals by year')
-    ))
-    st.subheader('What if you bet $100 on total goals of > 2.5 in all ' + add_selectbox + ' home games?')
-    st.text('You would got ' + str(selected_team_home['BetMore25'].sum().astype(int)) + ' USD') 
-    st.altair_chart(chart_home_selected, use_container_width=True)
-
-
-    chart_away_selected = alt.Chart(selected_team_away).mark_bar(size=10).encode(
-        x='Date',
-        y='BetLess25',
-        color=alt.condition(
-            alt.datum.BetLess25 > 0,
-            alt.value("green"),  # The positive color
-            alt.value("red")  # The negative color
-        #color='animal',
-        # legend=alt.Legend(title='Animals by year')
-    ))
-    st.subheader('What if you bet $100 on total goals of < 2.5 in all ' + add_selectbox + ' away games?')
-    st.text('You would got ' + str(selected_team_away['BetLess25'].sum().astype(int)) + ' USD') 
-    st.altair_chart(chart_away_selected, use_container_width=True)
+    with st.beta_expander("Home wins, draws and away wins bets"):
+        
     
-
-    chart_away_selected = alt.Chart(selected_team_away).mark_bar(size=10).encode(
-        x='Date',
-        y='BetMore25',
-        color=alt.condition(
-            alt.datum.BetMore25 > 0,
-            alt.value("green"),  # The positive color
-            alt.value("red")  # The negative color
-        #color='animal',
-        # legend=alt.Legend(title='Animals by year')
-    ))
-    st.subheader('What if you bet $100 on total goals of > 2.5 in all ' + add_selectbox + ' away games?')
-    st.text('You would got ' + str(selected_team_away['BetMore25'].sum().astype(int)) + ' USD') 
-    st.altair_chart(chart_away_selected, use_container_width=True)
-
-
-
-    #Goals at home games
-    chart_home_selected = alt.Chart(selected_team_home).mark_bar(size=10).encode(
-        x='Date',
-        y='TotalGoals')
-    rule = alt.Chart(selected_team_home).mark_rule(color='red').encode(y='mean(TotalGoals)')
-    st.subheader('Total goals in ' + add_selectbox + ' home games')
-    st.text('Red line shows the average (' + str(selected_team_home['TotalGoals'].mean()) + ' goals per game)') 
-    st.altair_chart(chart_home_selected + rule, use_container_width=True)
-
-    #Goals at away games
-    chart_away_selected = alt.Chart(selected_team_away).mark_bar(size=10).encode(
-        x='Date',
-        y='TotalGoals')
-    rule = alt.Chart(selected_team_away).mark_rule(color='red').encode(y='mean(TotalGoals)')
-    st.subheader('Total goals in ' + add_selectbox + ' away games')
-    st.text('Red line shows the average (' + str(selected_team_away['TotalGoals'].mean()) + ' goals per game)') 
-    st.altair_chart(chart_away_selected + rule, use_container_width=True)
+        chart_home_selected = alt.Chart(selected_team_home).mark_bar(size=10).encode(
+            x='Date',
+            y='BetWinHome',
+            color=alt.condition(
+                alt.datum.BetWinHome > 0,
+                alt.value("green"),  # The positive color
+                alt.value("red")  # The negative color
+            #color='animal',
+            # legend=alt.Legend(title='Animals by year')
+        ))
+        st.subheader('What if you bet $100 on home team wins in all ' + add_selectbox + ' home games?')
+        st.text('You would got ' + str(selected_team_home['BetWinHome'].sum().astype(int)) + ' USD') 
+        st.altair_chart(chart_home_selected, use_container_width=True)
+     
+    
+        # Basic Altair line chart where it picks automatically the colors for the lines
+        chart_draw_selected = alt.Chart(selected_team_draw).mark_bar(size=10).encode(
+            x='Date',
+            y='BetDraw',
+            color=alt.condition(
+                alt.datum.BetDraw > 0,
+                alt.value("green"),  # The positive color
+                alt.value("red")  # The negative color
+            #color='animal',
+            # legend=alt.Legend(title='Animals by year')
+        ))
+        st.subheader('What if you bet $100 on draws in all ' + add_selectbox + ' games?')
+        st.text('You would got ' + str(selected_team_draw['BetDraw'].sum().astype(int)) + ' USD')   
+        st.altair_chart(chart_draw_selected, use_container_width=True)
     
     
+        chart_away_selected = alt.Chart(selected_team_away).mark_bar(size=10).encode(
+            x='Date',
+            y='BetWinAway',
+            color=alt.condition(
+                alt.datum.BetWinAway > 0,
+                alt.value("green"),  # The positive color
+                alt.value("red")  # The negative color
+            #color='animal',
+            # legend=alt.Legend(title='Animals by year')
+        ))
+        st.subheader('What if you bet $100 on away team wins in all ' + add_selectbox + ' away games?')
+        st.text('You would got ' + str(selected_team_away['BetWinAway'].sum().astype(int)) + ' USD') 
+        st.altair_chart(chart_away_selected, use_container_width=True)
 
+    with st.beta_expander("Total Goals Bets with 2.5 goals threshold"):
+        
+        #st.header('Total Goals')
     
+        chart_home_selected = alt.Chart(selected_team_home).mark_bar(size=10).encode(
+            x='Date',
+            y='BetLess25',
+            color=alt.condition(
+                alt.datum.BetLess25 > 0,
+                alt.value("green"),  # The positive color
+                alt.value("red")  # The negative color
+            #color='animal',
+            # legend=alt.Legend(title='Animals by year')
+        ))
+        st.subheader('What if you bet $100 on total goals of < 2.5 in all ' + add_selectbox + ' home games?')
+        st.text('You would got ' + str(selected_team_home['BetLess25'].sum().astype(int)) + ' USD') 
+        st.altair_chart(chart_home_selected, use_container_width=True)
+    
+    
+        chart_home_selected = alt.Chart(selected_team_home).mark_bar(size=10).encode(
+            x='Date',
+            y='BetMore25',
+            color=alt.condition(
+                alt.datum.BetMore25 > 0,
+                alt.value("green"),  # The positive color
+                alt.value("red")  # The negative color
+            #color='animal',
+            # legend=alt.Legend(title='Animals by year')
+        ))
+        st.subheader('What if you bet $100 on total goals of > 2.5 in all ' + add_selectbox + ' home games?')
+        st.text('You would got ' + str(selected_team_home['BetMore25'].sum().astype(int)) + ' USD') 
+        st.altair_chart(chart_home_selected, use_container_width=True)
+    
+    
+        chart_away_selected = alt.Chart(selected_team_away).mark_bar(size=10).encode(
+            x='Date',
+            y='BetLess25',
+            color=alt.condition(
+                alt.datum.BetLess25 > 0,
+                alt.value("green"),  # The positive color
+                alt.value("red")  # The negative color
+            #color='animal',
+            # legend=alt.Legend(title='Animals by year')
+        ))
+        st.subheader('What if you bet $100 on total goals of < 2.5 in all ' + add_selectbox + ' away games?')
+        st.text('You would got ' + str(selected_team_away['BetLess25'].sum().astype(int)) + ' USD') 
+        st.altair_chart(chart_away_selected, use_container_width=True)
+        
+    
+        chart_away_selected = alt.Chart(selected_team_away).mark_bar(size=10).encode(
+            x='Date',
+            y='BetMore25',
+            color=alt.condition(
+                alt.datum.BetMore25 > 0,
+                alt.value("green"),  # The positive color
+                alt.value("red")  # The negative color
+            #color='animal',
+            # legend=alt.Legend(title='Animals by year')
+        ))
+        st.subheader('What if you bet $100 on total goals of > 2.5 in all ' + add_selectbox + ' away games?')
+        st.text('You would got ' + str(selected_team_away['BetMore25'].sum().astype(int)) + ' USD') 
+        st.altair_chart(chart_away_selected, use_container_width=True)
 
-
-st.text('AvgH = Market average home win odds')
-st.text('AvgD = Market average draw win odds')
-st.text('AvgA = Market average away win odds')
-st.text('Avg>2.5 = Market average over 2.5 goals')
-st.text('Avg<2.5 = Market average under 2.5 goals')
+    with st.beta_expander("See total goals statistics"):
+    
+        #Goals at home games
+        chart_home_selected = alt.Chart(selected_team_home).mark_bar(size=10).encode(
+            x='Date',
+            y='TotalGoals')
+        rule = alt.Chart(selected_team_home).mark_rule(color='red').encode(y='mean(TotalGoals)')
+        st.subheader('Total goals in ' + add_selectbox + ' home games')
+        st.text('Red line shows the average (' + str(selected_team_home['TotalGoals'].mean()) + ' goals per game)') 
+        st.altair_chart(chart_home_selected + rule, use_container_width=True)
+    
+        #Goals at away games
+        chart_away_selected = alt.Chart(selected_team_away).mark_bar(size=10).encode(
+            x='Date',
+            y='TotalGoals')
+        rule = alt.Chart(selected_team_away).mark_rule(color='red').encode(y='mean(TotalGoals)')
+        st.subheader('Total goals in ' + add_selectbox + ' away games')
+        st.text('Red line shows the average (' + str(selected_team_away['TotalGoals'].mean()) + ' goals per game)') 
+        st.altair_chart(chart_away_selected + rule, use_container_width=True)
+    
